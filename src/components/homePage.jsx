5bf383150d6b269abad9e22bd660c3bf62ed3872
Sabaris <sabaris.elango@gmail.com>
2024-11-09 05:14:01 +0530

    Add header content sec to component

--- src/components/homePage.jsx
+++ src/components/homePage.jsx
@@ -3,6 +3,74 @@ import { useState, useEffect } from "react";
 const HeadTitle = () => {
   const [score, setScore] = useState(0);
   const [bestScore, setBestScore] = useState(0);
+  const [newGame, setNewGame] = useState(false);
+  const [pokemonData, setPokemonData] = useState([]);
+  const [pokemonCheck, setPokemonCheck] = useState([]);
+
+  useEffect(() => {
+    const pushPokemonData = async (temp) => {
+      let temp2 = [];
+      try {
+        const res = await fetch(
+          "https://pokeapi-proxy.freecodecamp.rocks/api/pokemon"
+        );
+        const data = await res.json();
+        for (let i = 0; i < 12; i++) {
+          const res2 = await fetch(data.results[temp[i]].url);
+          const data2 = await res2.json();
+          temp2.push(data2);
+        }
+        setPokemonData([...temp2]);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    let temp = [];
+
+    for (let i = 0; i < 12; i++) {
+      // 1302 no of pokemons 1 - 1302
+      let ignore = false;
+
+      while (!ignore) {
+        let pokId = Math.floor(Math.random() * 1302);
+        if (!temp.includes(pokId)) {
+          temp.push(pokId);
+          ignore = true;
+        }
+      }
+    }
+    pushPokemonData(temp);
+    setNewGame(false);
+  }, [newGame]);
+
+  const shuffleArray = () => {
+    let array = [...pokemonData];
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+    setPokemonData([...array]);
+  };
+
+  const handleClick = (pokemonId) => {
+    if (!pokemonCheck.includes(pokemonId)) {
+      let temp = [...pokemonCheck];
+      temp.push(pokemonId);
+      setPokemonCheck([...temp]);
+      shuffleArray();
+      setScore(score + 1);
+      if (score >= bestScore) {
+        setBestScore(bestScore + 1);
+      }
+    } else {
+      setPokemonData([]);
+      setPokemonCheck([]);
+      setScore(0);
+      setNewGame(true);
+    }
+  };
 
   return (
     <div>
@@ -17,6 +85,17 @@ const HeadTitle = () => {
         <div>{score}</div>
         <div>{bestScore}</div>
       </div>
+      {pokemonData.map((pokemon) => {
+        return (
+          <div key={pokemon.id} onClick={() => handleClick(pokemon.id)}>
+            <div id={pokemon.id}>{pokemon.name}</div>
+            <img
+              src={pokemon.sprites.front_default}
+              alt={`${pokemon.name} img`}
+            />
+          </div>
+        );
+      })}
     </div>
   );
 };
